99efe3af1610ffbde0d9f3e15bb0dd0cd64cd156
Pouya Arvandian <pouya314@gmail.com>
2014-10-24 14:16:12 +0800

    runner (which is run by our simple command line interface) sends the command to Command Center for further processing. and catches any exception that might be raised.

--- lib/robogame/runner.rb
+++ lib/robogame/runner.rb
@@ -2,12 +2,27 @@ require_relative "commands"
 
 module Robogame
   class Runner
-    def initialize
-      
-    end
+    # def initialize
+    #   
+    # end
     
     def run
-      puts "runner ran!"
+      command_centre = Commands.new
+
+      prompt = '> '
+      puts prompt
+
+      while command = STDIN.gets.chomp!.upcase
+        break if command == 'QUIT'
+        
+        begin
+          output = command_centre.execute(command)
+          puts output if output
+        rescue Exception => e
+          puts "#{e.class}: #{e.message}"
+        end
+        puts prompt
+      end
     end
   end
 end
