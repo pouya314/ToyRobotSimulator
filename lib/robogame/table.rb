8aa3d2dd8047635456c0d938cb4915a799ed20e6
Pouya Arvandian <pouya314@gmail.com>
2014-10-24 17:39:48 +0800

    initiate minimum values for x,y as well. provide a method for callers to query validity of coordinates before they take certain actions (e.g. move, place, etc)

--- lib/robogame/table.rb
+++ lib/robogame/table.rb
@@ -1,13 +1,14 @@
 module Robogame
   class Table
     def initialize(max_x, max_y)
+      @min_x = 0
+      @min_y = 0
       @max_x = max_x
       @max_y = max_y
-      @robot = nil
     end
     
-    def place_robot(robot, x, y, f)
-      
+    def coordinates_valid?(x,y)
+      x < @min_x || x > @max_x || y < @min_y || y > @max_y ? false : true
     end
   end
 end
