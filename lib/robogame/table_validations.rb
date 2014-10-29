b20b8c4742d4d9ae49b0c06a0ba852b1e49b4151
Pouya Arvandian <pouya314@gmail.com>
2014-10-29 22:33:20 +0800

    Update Table validation.
    Use a more intuitive error message.

--- lib/robogame/table_validations.rb
+++ lib/robogame/table_validations.rb
@@ -2,7 +2,7 @@ require_relative 'errors'
 
 module  TableValidations
   def check_coordinates(x,y)
-    raise InvalidCoordinates, "Invalid Coordinates!" if x < @min_x || x > @max_x || y < @min_y || y > @max_y
+    raise InvalidCoordinates, "Out of boundry!" if x < @min_x || x > @max_x || y < @min_y || y > @max_y
     super
   end
 end
