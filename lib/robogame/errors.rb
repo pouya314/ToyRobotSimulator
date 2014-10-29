691c4508e573eab65a90455681e105ba8db9fb4f
Pouya Arvandian <pouya314@gmail.com>
2014-10-29 22:26:35 +0800

    update errors.
    - rename error class name for better clarity.
    - introduce a new class of error : x,y coordinates not of type integer.

--- lib/robogame/errors.rb
+++ lib/robogame/errors.rb
@@ -3,11 +3,12 @@ class GameException < Exception; end
 # Command errors
 class EmptyCmd < GameException; end
 class InvalidCmd < GameException; end
-class InvalidPlaceCmdArgs < GameException; end
+class WrongNoOfArgs < GameException; end
 
 # Robot Errors
 class InvalidRobotDirection < GameException; end
 class RobotNotPlaced < GameException; end
+class CoordinatesNotInteger < GameException; end
 
 # Table Errors
 class InvalidCoordinates < GameException; end
